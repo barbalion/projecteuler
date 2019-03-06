8a1a69145976b586978dba73381488464a30cdaf
Alexander Ermakov <barbalion@gmail.com>
2019-03-06 20:49:53 +0300

    94: effective way this time

--- 94.py
+++ 94.py
@@ -1,27 +1,11 @@
-from itertools import count
-import math
+def calcSquarePerim(a, d):
+  ar = (a * 3 + d)*(a - d) # core to check from Heron's formula 
+  return a * 3 + d if int(ar ** .5) ** 2 == ar else 0
 
 maxp = 1000000000
+res = 0
+for a in range(2, int(maxp ** .5 / 3) + 1):
+  # check only the proper candidates (were found empirically, however can be strictly proven)
+  res += calcSquarePerim(a ** 2 + 1, 1) + calcSquarePerim(a ** 2 * 2 - 1, -1)
 
-ap1s = set() # store a, a, a+1
-am1s = set() # store a, a, a-1
-
-# Heron's formula for integer colculation, return area^2*16
-def area216(a, b, c):
-  p = (a + b + c)
-  return p*(p-2*a)*(p-2*b)*(p-2*c)
-
-def check(a, d, s):
-  ar = area216(a, a, a+d)
-  t = int(math.sqrt(ar // 16))
-  if t * t * 16 == ar:
-    s.add(a) 
-    print(a, d, t)
-
-for a in range(2, maxp // 3 + 1):
-  check(a, 1, ap1s)
-  check(a, -1, am1s)
-  if a % 1000000 == 0:
-    print(a)
-
-print(sum([a*3+1 for a in ap1s])+sum([a*3-1 for a in am1s]))
+print(res)
