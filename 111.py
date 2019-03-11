660d69ce8c0beac5aff313115c574a118128b4db
Alexander Ermakov <barbalion@gmail.com>
2019-03-11 23:53:14 +0300

    111: optimized

--- 111.py
+++ 111.py
@@ -1,7 +1,16 @@
 from tools import *
-import time
-t = time.perf_counter()
-tt = time.perf_counter()
+from functools import reduce
+
+def perm(p, n, nn):
+  if len(p) == n:
+    return [p]
+  return sum([perm(p[0:i]+[d]+p[i:], n, range(i+1, len(p) + 2)) for d in range(10) for i in nn], [])
+
+def primesWithMDs(n, m, d):
+  for c in perm([d] * m, n, range(m + 1)):
+    p = reduce(lambda a, b: a * 10 + b, c)
+    if c[0] > 0 and isPrime(p):
+      yield p
 
 def calc(n):
   minp = 10 ** (n-1)
@@ -11,34 +20,18 @@ def calc(n):
   M = [0] * 10
   N = [0] * 10
   S = [0] * 10
-  def countDigitsIn(p):
-    global t
-    for nn in range(n):
-      di = 10**(n-nn-1)
-      dt = p // di
-      p -= dt * di
-      cnt[dt] += 1
-    return cnt
-
-  for p in primesN2M(minp, maxp, 100000000):
-    global t
-    if time.perf_counter() - t > 1:
-      print(p, int(time.perf_counter() - tt), (time.perf_counter() - tt) / (p-minp)  * (maxp-minp))
-      t = time.perf_counter()
-    cnt = [0] * 10
-    for (d, c) in zip(range(10), countDigitsIn(p)):
-      if c > M[d]:
-        M[d] = c
-        N[d] = 0
-        S[d] = 0
-      if c == M[d]:
+  
+  for d in range(10):
+    for m in range(n-1, 0, -1):
+      for p in primesWithMDs(n, m, d):
+        M[d] = m
         N[d] += 1
         S[d] += p
+      if M[d] > 0:
+        break 
   return list(zip(range(10), M, N, S))
 
 n = 10
 stat = calc(n)
-print(n, stat)
+#print(n, stat)
 print(sum([s for (d, m, n, s) in stat]))
-
-print(time.perf_counter() - tt)
