4603de6fdeb29b50157d13c18016a55a4b993eeb
Alexander Ermakov <barbalion@gmail.com>
2019-03-12 14:18:24 +0300

    111: refactor

--- 111.py
+++ 111.py
@@ -1,37 +1,20 @@
 from tools import *
 from functools import reduce
 
-def perm(p, n, nn):
-  if len(p) == n:
-    return [p]
-  return sum([perm(p[0:i]+[d]+p[i:], n, range(i+1, len(p) + 2)) for d in range(10) for i in nn], [])
+def perm(p, n, positions): # all permutations of N-digit number containing all P digits
+  return [p] if len(p) == n else sum([perm(p[0:i]+[d]+p[i:], n, range(i+1, len(p) + 2)) for d in range(10) for i in positions], [])
 
-def primesWithMDs(n, m, d):
-  for c in perm([d] * m, n, range(m + 1)):
-    p = reduce(lambda a, b: a * 10 + b, c)
-    if c[0] > 0 and isPrime(p):
-      yield p
+def primesWithMDs(n, m, d): # all prime N-digit numbers with M 'D's.
+  return [p for p in [reduce(lambda a, b: a * 10 + b, c) for c in perm([d] * m, n, range(m + 1)) if c[0] > 0] if isPrime(p)]
 
 def calc(n):
-  minp = 10 ** (n-1)
-  maxp = minp * 10
-  cnt = 0
-
-  M = [0] * 10
-  N = [0] * 10
   S = [0] * 10
-  
   for d in range(10):
     for m in range(n-1, 0, -1):
       for p in primesWithMDs(n, m, d):
-        M[d] = m
-        N[d] += 1
         S[d] += p
-      if M[d] > 0:
+      if S[d] > 0:
         break 
-  return list(zip(range(10), M, N, S))
+  return S
 
-n = 10
-stat = calc(n)
-#print(n, stat)
-print(sum([s for (d, m, n, s) in stat]))
+print(sum(calc(10)))
