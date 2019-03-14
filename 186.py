3f430a91bd3be83a2839ef5e260a3851ec32e0d1
Alexander Ermakov <barbalion@gmail.com>
2019-03-14 20:49:32 +0300

    186: 30min

--- 186.py
+++ 186.py
@@ -1,6 +1,6 @@
 maxCalls = 10000000
 userCount = 1000000
-needFriends = userCount - userCount // 100 # 99%
+needFriends = userCount - userCount // 100 # >= 99%
 k = 0
 S = [0] * (maxCalls + 1)
 def laggedFib():
@@ -19,86 +19,65 @@ def records():
   while True:
     yield (next(nums), next(nums))
 
-def join(u1s, u2s):
-  if not u1s:
-    return u2s
-  for i in range(len(u1s)):
-    u1 = u1s[i]
-    u2 = u2s[i]
-    if u2:
-      if not u1:
-        u1 = u2
-      else:
-        u1.update(u2)
-      for f in list(u2):
-        if u1s[f]:
-          u1.update(u1s[f])
-        u1s[f] = u1
-
-  return u1s
-
-def calc(pm, mcalls):
+def calc(pm):
   users = [None] * userCount
   calls = 0
   maxLen = 0
-  gr = 0
-  gr1 = 0
-  gr2 = 0
-  dup = 0
+  gr, gr1, gr2, dup = 0, 0, 0, 0 # stat
   for u1, u2 in records():
-    if calls >= mcalls:
-      return users
-    if users[pm] and len(users[pm]) >= needFriends:
-      print(f'PM is a common friend after {calls} calls')
-      return 
     if u1 == u2:
       continue
     calls += 1     
     if calls % 1000 == 0:
-      print(f'calls={calls} gr={gr} (+{gr1}-{gr2}) dup={dup} maxLen={maxLen} pm friends={len(users[pm]) if users[pm] else 0} time={time.perf_counter() - t:0.3f}')
+      print(f'calls={calls} gr={gr} (+{gr1}-{gr2}) dup={dup} maxLen={maxLen} pm_friends={len(users[pm]) if users[pm] else 0} time={time.perf_counter() - t:0.3f}')
       gr1 = 0
       gr2 = 0
       dup = 0
+    if pm in [u1, u2]:
+      print(f'PM got a friend after {calls} calls')
 
     g1 = users[u1]
     g2 = users[u2]
     if g1 and g1 == g2:
-      dup += 1
+      dup += 1 # stat
       continue
 
     if not g1:
-      g1 = set([u1])
-      gr += 1
-      gr1 += 1
+      g1 = [u1]
       users[u1] = g1
+      gr += 1 # stat
+      gr1 += 1 # stat
     if g2:
       if len(g2) > len(g1):
         g1, g2 = g2, g1
         u1, u2 = u2, u1
-      g1.update(g2)
-      gr2 += 1
-      gr -= 1
+      g1 += g2
+      gr2 += 1 # stat
+      gr -= 1 # stat
       for u in g2:
         users[u] = g1
     else:
-      g1.add(u2)
+      g1 += [u2]
       users[u2] = g1
 
     maxLen = max(maxLen, len(g1))
+    if g1 == users[pm]:
+      print(f'New friend for pm: {len(g1)} at call {calls}')
+      g1.sort()
+      cnt = 0
+      pu = -1
+      for u in g1:
+        if u != pu:
+          cnt += 1
+          pu = u
+      print(f'        this gives {cnt} unique friends')
+      if cnt >= needFriends:
+        print(f'PM is a common friend ({cnt}) after {calls} calls')
+        return 
 
 import time
 t = time.perf_counter()
 
-maxs = maxCalls
-step = maxs #5000000
-u = []
-for calls in range(0, maxs, step):
-  u = join(u, calc(524287, step))
-  mlen = max([len(g) for g in u if g])
-  print(mlen)
-
-#for g in u:
-#  if g and len(g) == mlen:
-#    print(g)
+calc(524287)
 
 print(f'elapsed_time={time.perf_counter() - t:0.3f}')
