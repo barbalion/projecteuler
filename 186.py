94e011f61a473804a28e22d20da62f74b183b3f5
Alexander Ermakov <barbalion@gmail.com>
2019-03-14 19:48:05 +0300

    186: brute force

--- 186.py
+++ 186.py
@@ -1,26 +1,44 @@
+maxCalls = 10000000
+userCount = 1000000
+needFriends = userCount - userCount // 100 # 99%
+k = 0
+S = [0] * (maxCalls + 1)
 def laggedFib():
-  S = [0] * 10000000
-  for k in range(1, min(56, len(S))):
-    S[k] = (100003 - 200003 * k + 300007 * k**3) % 1000000
-    yield S[k]
-  k = 56
+  global k, S
   while True:
-    S[k] = (S[k-24] + S[k-55]) % 1000000
-    yield S[k]
     k += 1
+    if S[k] == 0:
+      if k <= 55:
+        S[k] = (100003 - 200003 * k + 300007 * k**3) % 1000000
+      else:
+        S[k] = (S[k-24] + S[k-55]) % 1000000
+    yield S[k]
 
 def records():
-  n = 1
   nums = laggedFib()
   while True:
     yield (next(nums), next(nums))
-    #from random import randint
-    #yield (randint(0, 99999), randint(0, 99999))
-    n += 1
 
+def join(u1s, u2s):
+  if not u1s:
+    return u2s
+  for i in range(len(u1s)):
+    u1 = u1s[i]
+    u2 = u2s[i]
+    if u2:
+      if not u1:
+        u1 = u2
+      else:
+        u1.update(u2)
+      for f in list(u2):
+        if u1s[f]:
+          u1.update(u1s[f])
+        u1s[f] = u1
 
-def calc(pm):
-  users = [None] * 1000000 #for i in range(1000000)]
+  return u1s
+
+def calc(pm, mcalls):
+  users = [None] * userCount
   calls = 0
   maxLen = 0
   gr = 0
@@ -28,11 +46,16 @@ def calc(pm):
   gr2 = 0
   dup = 0
   for u1, u2 in records():
+    if calls >= mcalls:
+      return users
+    if users[pm] and len(users[pm]) >= needFriends:
+      print(f'PM is a common friend after {calls} calls')
+      return 
     if u1 == u2:
       continue
     calls += 1     
     if calls % 1000 == 0:
-      print(f'calls={calls}, gr={gr} (+{gr1}-{gr2}), dup={dup}, maxLen={maxLen}')
+      print(f'calls={calls} gr={gr} (+{gr1}-{gr2}) dup={dup} maxLen={maxLen} pm friends={len(users[pm]) if users[pm] else 0} time={time.perf_counter() - t:0.3f}')
       gr1 = 0
       gr2 = 0
       dup = 0
@@ -49,6 +72,9 @@ def calc(pm):
       gr1 += 1
       users[u1] = g1
     if g2:
+      if len(g2) > len(g1):
+        g1, g2 = g2, g1
+        u1, u2 = u2, u1
       g1.update(g2)
       gr2 += 1
       gr -= 1
@@ -60,12 +86,19 @@ def calc(pm):
 
     maxLen = max(maxLen, len(g1))
 
-    if calls == 5000000:
-      return
-
 import time
 t = time.perf_counter()
 
-calc(524287)
+maxs = maxCalls
+step = maxs #5000000
+u = []
+for calls in range(0, maxs, step):
+  u = join(u, calc(524287, step))
+  mlen = max([len(g) for g in u if g])
+  print(mlen)
+
+#for g in u:
+#  if g and len(g) == mlen:
+#    print(g)
 
 print(f'elapsed_time={time.perf_counter() - t:0.3f}')
