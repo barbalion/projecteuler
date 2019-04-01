e2bb193c8801c0b29cfaa92b7a9b5f9d7c72c444
Alexander Ermakov <barbalion@gmail.com>
2019-04-01 19:21:11 +0300

    224: optimization: 10 secs

--- 224.py
+++ 224.py
@@ -3,14 +3,13 @@ from tools import *
 def cnt(q, P):
   res = 0
   while q:
-    a, b, c = q.pop(-1)
+    a, b, c = q.pop()
     while a + b + c <= P:
       res += 1
-      q += [
-        (2*a + b + 2*c,a + 2*b + 2*c,2*a + 2*b + 3*c),
-      ] + ([
-        (a - 2*b + 2*c,2*a - b + 2*c,2*a - 2*b + 3*c)
-      ] if a != b else [])
+      if 5*(a + b) + 7*c <= P:
+        q.append((2*a + b + 2*c,a + 2*b + 2*c,2*a + 2*b + 3*c))
+      if a != b and 5*(a - b) + 7*c <= P:
+        q.append((a - 2*b + 2*c,2*a - b + 2*c,2*a - 2*b + 3*c))
       a, b, c = -2*a + b + 2*c,-a + 2*b + 2*c,-2*a + 2*b + 3*c
   return res
 
