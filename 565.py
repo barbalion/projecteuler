1d3597c92b11634714d56bcbb80e5ac5168e8fc5
Alexander Ermakov <barbalion@gmail.com>
2019-04-04 20:27:28 +0300

    565: smarter code: ~100 hours

--- 565.py
+++ 565.py
@@ -1,17 +1,50 @@
 from tools import *
 
-def calc(N, D):
-  if N > 1000:
-    cachePrimes(int(N**.5))
+def sig(f):
+  P = 1
+  for p,a in f:
+    P *= (p**(a+1)-1) // (p-1)
+  return P
+
+def check(n, D):
+  return 0 if sig(factor(n)) % D else n
+
+def calc2(N, D, e=0):
   res = 0
-  for n in range(1, N+1):
-    ds = divs(n)
-    s = sum(ds)
+  for p in primesN2M(2, int(N**(1/e))+1):
+    a = p**e
+    s = (p**(e+1)-1) // (p-1)
     if s % D == 0:
-      res += n
-      print(n, res, n/D, n%D)
-    if n % 100000 == 0:
-      elapsed((n, 1, N))
+      k = N // a
+      res += a*k*(k + 1) // 2
+      j = k//p
+      res -= a*a*j*(j+1)//2
   return res
 
-print(calc(10**7, 2017))
+def calc(N, D):
+  cachePrimes(int(N**.5))
+  res = 0
+  for e in range(2, 5):
+    res += calc2(N, D, e)
+
+  ex = set()
+
+  maxN = N//D
+  for n in range(6, maxN+1):
+    a = n*D - 1
+    if isPrime(a):
+      k = N // a
+      res += a*k*(k + 1) // 2
+
+      for kk in range(a, k+1):
+        if check(kk, D) > 1 and not (a*kk in ex):
+          res -= a*kk
+          ex.add(a*kk)
+
+    if n % 10000 == 1:
+      elapsed((n, 1, maxN))
+  return res
+
+print(calc(10**11, 2017))
+
+elapsed()
