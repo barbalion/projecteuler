093255d7da8a66199ef8c951bf18ce2a59a9a1a8
Alexander Ermakov <barbalion@gmail.com>
2019-04-06 22:24:29 +0300

    565: solved in 10mins

--- 565.py
+++ 565.py
@@ -1,50 +1,41 @@
+# 217 is a prime number. In order 2017 to be a divisor of for sigma(a)=Product((p**(n+1)-1) // (p-1) for all divisors) it must be a power of a prime with (p**(a+1)-1) // (p-1) % 2017 == 0
+# so we need only to check all such primes 'p' and count all k*p for k=1..N//p. We check p^n with n>=2 by brute force all primes. For n = 1 we generate all candidates by the formula p = k*2017-1 and check if it's a prime in a effective way.
+# we also take care of duplicates when p'=k*p is not a proper number if k is a proper number itself.
 from tools import *
-
-def sig(f):
-  P = 1
-  for p,a in f:
-    P *= (p**(a+1)-1) // (p-1)
-  return P
-
-def check(n, D):
-  return 0 if sig(factor(n)) % D else n
-
-def calc2(N, D, e=0):
-  res = 0
-  for p in primesN2M(2, int(N**(1/e))+1):
-    a = p**e
-    s = (p**(e+1)-1) // (p-1)
-    if s % D == 0:
-      k = N // a
-      res += a*k*(k + 1) // 2
-      j = k//p
-      res -= a*a*j*(j+1)//2
-  return res
 
 def calc(N, D):
   cachePrimes(int(N**.5))
+  def a2n(a):
+    return (a + 1) // D
+  def genPrimeSigDivs(n, m): # generate all primes and primes**k with sig % 2017 == 0
+    if n > m:
+      return None
+    for e in range(2, 5): # for power > 2 check all primes
+      for p in primesN2M(int((n*D-1)**(1/e))+1, int((m*D-1)**(1/e))):
+        a = p**e
+        if ((p**(e+1)-1) // (p-1)) % D == 0:
+          yield a
+    for n in range(n, m+1): # for power==1 check all 2017*k-1 for primality
+      a = n * D - 1
+      if isPrimeMillerRabin(a): 
+        yield a
+
+  elapsed()
+  resetTime()
+
   res = 0
-  for e in range(2, 5):
-    res += calc2(N, D, e)
+  for a in genPrimeSigDivs(1, a2n(N)): # walk though all proper primes**n
+    k = N // a # count of rated numbers
+    res += a*k*(k + 1) // 2 # sum of all rated numbers
 
-  ex = set()
+    for aa in genPrimeSigDivs(a2n(a), a2n(k)):
+      for dup in range(0, N, aa*a):
+        res -= dup
 
-  maxN = N//D
-  for n in range(6, maxN+1):
-    a = n*D - 1
-    if isPrime(a):
-      k = N // a
-      res += a*k*(k + 1) // 2
-
-      for kk in range(a, k+1):
-        if check(kk, D) > 1 and not (a*kk in ex):
-          res -= a*kk
-          ex.add(a*kk)
-
-    if n % 10000 == 1:
-      elapsed((n, 1, maxN))
+    if a % 1000 == 1:
+      elapsed((a, 1, N))
   return res
 
-print(calc(10**11, 2017))
+print(calc(10**9, 2017))
 
 elapsed()
